d3960fe772edc70ebba617d2c553220799a2408a
teivan1 <ivanchuas2012@gmail.com>
2026-08-13 15:17:01 -0300

    refactor: saque un main inecesario

--- src/test/java/ar/edu/unrc/game2048/CellTest.java
+++ src/test/java/ar/edu/unrc/game2048/CellTest.java
@@ -9,12 +9,9 @@ public class CellTest {
     @Test
     public void testConstructor() {
         assertThrows(IllegalArgumentException.class, () -> {
-            ar.edu.unrc.game2048.Cell cell1 = new ar.edu.unrc.game2048.Cell(-1);
+            Cell cell1 = new Cell(-1);
         });  
     }
 
-    public static void main(String[] args) {
-        CellTest cellTest1 = new CellTest();
-        cellTest1.testConstructor();
-    }
+
 }
