e11cac2e70d745c5e61d17425958a6d6829cad0f
teivan1 <ivanchuas2012@gmail.com>
2026-08-13 15:33:03 -0300

    test: tres tests para la clase Cell

--- src/test/java/ar/edu/unrc/game2048/CellTest.java
+++ src/test/java/ar/edu/unrc/game2048/CellTest.java
@@ -2,7 +2,7 @@ package ar.edu.unrc.game2048;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CellTest {
 
@@ -13,5 +13,32 @@ public class CellTest {
         });  
     }
 
+    @Test
+    public void testEmpty() {
+        Cell cell1 = new Cell(0);
+        assertTrue(cell1.isEmpty());
 
+        Cell cell2= new Cell(2);
+        assertFalse(cell2.isEmpty());
+    }
+
+    @Test
+    public void testGetValue(){
+        Cell cell1 = new Cell(2);
+        assertEquals(2, cell1.getValue());
+
+        assertNotEquals(4, cell1.getValue());
+    }
+
+    @Test
+    public void testCanMergeWith(){
+        Cell cell1 = new Cell(2);
+        Cell cell2 = new Cell(2);
+        Cell cell3 = new Cell(4);
+        Cell cell4 = new Cell(0);
+
+        assertTrue(cell1.canMergeWith(cell2));
+        assertFalse(cell1.canMergeWith(cell3));
+        assertFalse(cell1.canMergeWith(cell4));
+    }
 }
